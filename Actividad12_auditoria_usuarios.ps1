609d0e01e967280faae89b06445f1fce90036bf5
Misdra-code <brandon.alvarezardn@uanl.edu.mx>
2026-03-25 21:30:46 -0600

    Update Actividad12_auditoria_usuarios.ps1

--- Actividad12_auditoria_usuarios.ps1
+++ Actividad12_auditoria_usuarios.ps1
@@ -9,12 +9,12 @@ foreach ($u in $usuarios) {
         }
 }
 #Guardar en archivos separados
-$sinLogon | Out-File -FilePath "C:\Users\yumel\OneDrive\Escritorio\PS\Actividad12_usuarios_sin_logon.txt"
-$conLogon | Out-File -FilePath "C:\Users\yumel\OneDrive\Escritorio\PS\Actividad12_usuarios_con_logon.txt"
+$sinLogon | Out-File -FilePath "C:\Users\user\Escritorio\PS\Actividad12_usuarios_sin_logon.txt"
+$conLogon | Out-File -FilePath "C:\Users\user\Escritorio\PS\Actividad12_usuarios_con_logon.txt"
 #Mostrar en pantalla
 $reporte = @()
 $reporte += Write-Output "`n Usuarios que NUNCA han iniciado sesión:"
 $reporte += $sinLogon | ForEach-Object {Write-Output $_}
 $reporte += Write-Output "`n Usuarios que SÍ han iniciado sesión"
 $reporte += $conLogon | ForEach-Object {Write-Output $_}
-$reporte | Out-File -FilePath "C:\Users\yumel\OneDrive\Escritorio\PS\Actividad12_reporte_usuarios.txt"
+$reporte | Out-File -FilePath "C:\Users\user\Escritorio\PS\Actividad12_reporte_usuarios.txt"
